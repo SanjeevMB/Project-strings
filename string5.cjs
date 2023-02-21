ce31e2d642740d497b3200efdb6530468fe86acd
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-02-21 18:52:01 +0530

    string5.cjs ./test/testString5.cjs added
    solevd and tested

---
+++ string5.cjs
@@ -0,0 +1,11 @@
+
+// ==== String Problem #5 ====
+
+// Given an array of strings ["the", "quick", "brown", "fox"], convert it into a string "the quick brown fox."
+// If the array is empty, return an empty string.
+
+function string5(arr){
+    return arr.join(' ');
+}
+
+module.exports = string5;
---
+++ test/testString5.cjs
@@ -0,0 +1,7 @@
+let string5 = require('../string5.cjs');
+
+let testArr = ["the", "quick", "brown", "fox", "jumps", "over", "the", "the", "lazy", "dog"];
+
+let result = string5(testArr);
+
+console.log(result);
